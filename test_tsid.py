64e5fcfea5a2d6dac1fd9662c1eb0caea6179600
sduminy <sduminy@laas.fr>
2019-11-29 17:06:37 +0100

    test_tsid with a motion task for each foot

--- test_tsid.py
+++ test_tsid.py
@@ -26,7 +26,13 @@ dt = 0.001				# controller time step
 t = 0.0  				# time
 
 # Build empty lists to gather and plot the results
+zFRpos = []
+zFLpos = []
+zHRpos = []
 zHLpos = []
+err_zFR = []
+err_zFL = []
+err_zHR = []
 err_zHL = []
 
 
@@ -113,6 +119,33 @@ HLfootTask.setMask(mask)
 # Add the task to the HQP with weight = w_foot, priority level = 1 (in the cost function) and a transition duration = 0.0
 invdyn.addMotionTask(HLfootTask, w_foot, 1, 0.0)
 
+# HR foot
+HRfootTask = tsid.TaskSE3Equality("HR-foot-grounded", robot, 'HR_FOOT')
+HRfootTask.setKp(kp_foot * matlib.ones(6).T)
+HRfootTask.setKd(2.0 * np.sqrt(kp_foot) * matlib.ones(6).T)
+HRfootTask.useLocalFrame(False)
+HRfootTask.setMask(mask)
+# Add the task to the HQP with weight = 1.0, priority level = 1 (in the cost function) and a transition duration = 0.0
+invdyn.addMotionTask(HRfootTask, w_foot, 1, 0.0)
+
+# FL foot
+FLfootTask = tsid.TaskSE3Equality("FL-foot-grounded", robot, 'FL_FOOT')
+FLfootTask.setKp(kp_foot * matlib.ones(6).T)
+FLfootTask.setKd(2.0 * np.sqrt(kp_foot) * matlib.ones(6).T)
+FLfootTask.useLocalFrame(False)
+FLfootTask.setMask(mask)
+# Add the task to the HQP with weight = 1.0, priority level = 1 (in the cost function) and a transition duration = 0.0
+invdyn.addMotionTask(FLfootTask, w_foot, 1, 0.0)
+
+# FR foot
+FRfootTask = tsid.TaskSE3Equality("FR-foot-grounded", robot, 'FR_FOOT')
+FRfootTask.setKp(kp_foot * matlib.ones(6).T)
+FRfootTask.setKd(2.0 * np.sqrt(kp_foot) * matlib.ones(6).T)
+FRfootTask.useLocalFrame(False)
+FRfootTask.setMask(mask)
+# Add the task to the HQP with weight = 1.0, priority level = 0 (in the cost function) and a transition duration = 0.0
+invdyn.addMotionTask(FRfootTask, w_foot, 1, 0.0)
+
 
 ## TSID Trajectory
 
@@ -129,18 +162,48 @@ postureTask.setReference(samplePosture)
 pin.forwardKinematics(model, data, qdes)
 	
 HL_foot_ref = robot.framePosition(data, model.getFrameId('HL_FOOT'))
+HR_foot_ref = robot.framePosition(data, model.getFrameId('HR_FOOT'))
+FL_foot_ref = robot.framePosition(data, model.getFrameId('FL_FOOT'))
+FR_foot_ref = robot.framePosition(data, model.getFrameId('FR_FOOT'))
 
 tHL = HL_foot_ref.translation
-
-goals = { "HL":-0.15, "HR": 0.75, "FL": 0.80, "FR": 0.75 }
+tHR = HR_foot_ref.translation
+tFL = FL_foot_ref.translation
+tFR = FR_foot_ref.translation
+	
+goals = { "HL":-0.15, "HR": -0.15, "FL": -0.15, "FR": -0.15 }
 
 HL_foot_goal = HL_foot_ref.copy()
 HL_foot_goal.translation = np.matrix([tHL[0,0], tHL[1,0], goals["HL"]]).T
+
+FR_foot_goal = FR_foot_ref.copy()
+FR_foot_goal.translation = np.matrix([tFR[0,0], tFR[1,0], goals["FR"]]).T
+  
+FL_foot_goal = FL_foot_ref.copy()
+FL_foot_goal.translation = np.matrix([tFL[0,0], tFL[1,0], goals["FL"]]).T
+  
+HR_foot_goal = HR_foot_ref.copy()
+HR_foot_goal.translation = np.matrix([tHR[0,0], tHR[1,0], goals["HR"]]).T
 	
 trajHLfoot = tsid.TrajectorySE3Constant("traj_HL_foot", HL_foot_goal)
 
+trajHRfoot = tsid.TrajectorySE3Constant("traj_HR_foot", HR_foot_goal)
+	
+trajFLfoot = tsid.TrajectorySE3Constant("traj_FL_foot", FL_foot_goal)
+	
+trajFRfoot = tsid.TrajectorySE3Constant("traj_FR_foot", FR_foot_goal)
+
 sampleHLfoot = trajHLfoot.computeNext()
 HLfootTask.setReference(sampleHLfoot)
+
+sampleHRfoot = trajHRfoot.computeNext()
+HRfootTask.setReference(sampleHRfoot)
+
+sampleFLfoot = trajFLfoot.computeNext()
+FLfootTask.setReference(sampleFLfoot)
+
+sampleFRfoot = trajFRfoot.computeNext()
+FRfootTask.setReference(sampleFRfoot)
 
 
 ## Initialisation of the solver
@@ -158,9 +221,15 @@ for i in range (N_SIMULATION):
 	pin.forwardKinematics(model, data, qdes)
 	
 	HL_foot_ref = robot.framePosition(data, model.getFrameId('HL_FOOT'))
-	
-	tHL = HL_foot_ref.translation	
+	HR_foot_ref = robot.framePosition(data, model.getFrameId('HR_FOOT'))
+	FL_foot_ref = robot.framePosition(data, model.getFrameId('FL_FOOT'))
+	FR_foot_ref = robot.framePosition(data, model.getFrameId('FR_FOOT'))
 
+	tHL = HL_foot_ref.translation	
+	tHR = HR_foot_ref.translation
+	tFL = FL_foot_ref.translation
+	tFR = FR_foot_ref.translation
+	
 	HQPData = invdyn.computeProblemData(t, qdes, vdes)
 	
 	sol = solver.solve(HQPData)
@@ -180,25 +249,39 @@ for i in range (N_SIMULATION):
 	
 	## Tests/results
 	
+	zFRpos.append(tFR[2,0])
+	zFLpos.append(tFL[2,0])
+	zHRpos.append(tHR[2,0])
 	zHLpos.append(tHL[2,0])
 	
+	err_zFR.append(goals["FR"] - tFR[2,0])
+	err_zFL.append(goals["FL"] - tFL[2,0])
+	err_zHR.append(goals["HR"] - tHR[2,0])
 	err_zHL.append(goals["HL"] - tHL[2,0])
-
+	
 
 ## Plot the results
 
 import matplotlib.pylab as plt
 
-ts = np.linspace(0.0, N_SIMULATION*dt, N_SIMULATION)
+ts = np.linspace(0, N_SIMULATION*dt, N_SIMULATION)
 
 plt.figure(1)
-plt.plot(ts, zHLpos)
+plt.plot(ts, zFRpos, label='FR foot')
+plt.plot(ts, zFLpos, label='FL foot')
+plt.plot(ts, zHRpos, label='HR foot')
+plt.plot(ts, zHLpos, label='HL foot')
 plt.grid()
-plt.title('Position of the HL-foot along z-axis in time')
+plt.title('Position of each foot along z-axis function of time')
+plt.legend()
 
 plt.figure(2)
-plt.plot(ts, err_zHL)
+plt.plot(ts, err_zFR, label='FR foot')
+plt.plot(ts, err_zFL, label='FL foot')
+plt.plot(ts, err_zHR, label='HR foot')
+plt.plot(ts, err_zHL, label='HL foot')
 plt.grid()
-plt.title('Position error of the HL-foot along z_axis in time')
+plt.title('Position errors of each foot along z_axis function of time')
+plt.legend()
 plt.show()
 
