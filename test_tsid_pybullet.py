f20b528fbe7c1d575e94d3dfc83f5e533786aecd
sduminy <sduminy@laas.fr>
2019-12-06 14:35:16 +0100

    Switch to solo 8 DOF in PyBullet and adapt the control

--- test_tsid_pybullet.py
+++ test_tsid_pybullet.py
@@ -12,6 +12,10 @@ from IPython import embed
 
 pin.switchToNumpyMatrix()
 
+########################################################################
+#                          TSID Controller                             #
+########################################################################
+
 ## Initialization
 
 # Definition of the tasks gains and weights
@@ -21,10 +25,10 @@ w_forceRef = 1e-3		# weight of the forces regularization for the contacts
 w_lock = 10.0			# weight of the lock task
 
 kp_com = 100.0 			# proportionnal gain of the CoM task
-kp_posture = 1.0  		# proportionnal gain of the posture task
+kp_posture = 10.0  		# proportionnal gain of the posture task
 kd_posture = 10.0		# derivative gain of the posture task
 kp_contact = 0.0		# proportionnal gain of the contacts
-kp_lock = 10000.0		# proportionnal gain of the lock task
+kp_lock = 0.0			# proportionnal gain of the lock task
 
 # For the contacts
 mu = 0.3  		# friction coefficient
@@ -62,10 +66,7 @@ pin.loadReferenceConfigurations(model, srdf, False)
 
 # Set the current configuration q to the robot configuration straight_standing
 qdes = model.referenceConfigurations['straight_standing']
-
-# Set the current velocity to zero
 vdes = np.matrix(np.zeros(robot.nv)).T
-
 
 ## Display the robot in Gepetto Viewer
 
@@ -158,8 +159,8 @@ solver = tsid.SolverHQuadProgFast("qp solver")
 # Resize the solver to fit the number of variables, equality and inequality constraints
 solver.resize(invdyn.nVar, invdyn.nEq, invdyn.nIn)
 
-# Initialisation of the plot variables which will be updated during the simulation loop 
-# These variables describe the behavior of the CoM of the robot (reference and real position, velocity and acceleration)
+# Initialization of the plot variables which will be updated during the simulation loop 
+# These variables describe the acceleration of the shoulders joints
 acc_shoulder1 = matlib.empty((1, N_SIMULATION))
 acc_shoulder2 = matlib.empty((1, N_SIMULATION))
 acc_shoulder3 = matlib.empty((1, N_SIMULATION))
@@ -167,9 +168,12 @@ acc_shoulder4 = matlib.empty((1, N_SIMULATION))
 
 
 ########################################################################
+#                              PyBullet                                #
+########################################################################
+
 # Initialization of PyBullet variables 
 
-v_prev = np.matrix(np.zeros(robot.nv)).T  # velocity during the previous time step, of size (robot.nv,1)
+v_prev = np.matrix(np.zeros(robot.nv-4)).T  # velocity during the previous time step, of size (robot.nv,1)
 
 # Start the client for PyBullet
 physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
@@ -185,14 +189,14 @@ planeId = p.loadURDF("plane.urdf")
 robotStartPos = [0,0,0.25] 
 robotStartOrientation = p.getQuaternionFromEuler([0,0,0])
 p.setAdditionalSearchPath("/opt/openrobots/share/example-robot-data/robots/solo_description/robots")
-robotId = p.loadURDF("solo12.urdf",robotStartPos, robotStartOrientation)
+robotId = p.loadURDF("solo.urdf",robotStartPos, robotStartOrientation)
 
 # Disable default motor control for revolute joints
-revoluteJointIndices = [0,1,2, 4,5,6, 8,9,10, 12,13,14]
+revoluteJointIndices = [0,1, 3,4, 6,7, 9,10]
 p.setJointMotorControlArray(robotId, jointIndices = revoluteJointIndices, controlMode = p.VELOCITY_CONTROL,targetVelocities = [0.0 for m in revoluteJointIndices], forces = [0.0 for m in revoluteJointIndices])
 								 
 # Initialize the joint configuration
-initial_joint_positions = [0., 0.8, -1.6, 0., 0.8, -1.6, 0., -0.8, 1.6, 0., -0.8, 1.6]
+initial_joint_positions = [0.8, -1.6, 0.8, -1.6, -0.8, 1.6, -0.8, 1.6]
 for i in range (len(initial_joint_positions)):
 	p.resetJointState(robotId, revoluteJointIndices[i], initial_joint_positions[i])
 							
@@ -204,70 +208,63 @@ p.setJointMotorControlArray(robotId, revoluteJointIndices, controlMode=p.TORQUE_
 realTimeSimulation = True
 
 
-## Sort contacts points to get only one contact per foot ##
-def getContactPoint(contactPoints):
-	for i in range(0,len(contactPoints)):
-		# There may be several contact points for each foot but only one of them as a non zero normal force
-		if (contactPoints[i][9] != 0): 
-			return contactPoints[i]
-	return 0 # If it returns 0 then it means there is no contact point with a non zero normal force (should not happen) 
-
+########################################################################
+#                      Torque Control function                         #
+########################################################################
 
 ## Function called from the main loop which computes the inverse dynamic problem and returns the torques
 def callback_torques():
-	global sol, t, v_prev, q, qdes, vdes
-
+	global sol, t, v_prev, q
+	
+	## Data collection from PyBullet
+	
 	jointStates = p.getJointStates(robotId, revoluteJointIndices) # State of all joints
 	baseState   = p.getBasePositionAndOrientation(robotId)
 	baseVel = p.getBaseVelocity(robotId)
-
-	# Info about contact points with the ground
-	contactPoints_FL = p.getContactPoints(robotId, planeId, linkIndexA=2)  # Front left  foot 
-	contactPoints_FR = p.getContactPoints(robotId, planeId, linkIndexA=5)  # Front right foot 
-	contactPoints_HL = p.getContactPoints(robotId, planeId, linkIndexA=8)  # Hind  left  foot 
-	contactPoints_HR = p.getContactPoints(robotId, planeId, linkIndexA=11) # Hind  right foot 
-
-	# Sort contacts points to get only one contact per foot
-	contactPoints = []
-	contactPoints.append(getContactPoint(contactPoints_FL))
-	contactPoints.append(getContactPoint(contactPoints_FR))
-	contactPoints.append(getContactPoint(contactPoints_HL))
-	contactPoints.append(getContactPoint(contactPoints_HR))
-
+	
 	# Joint vector for Pinocchio
 	q = np.vstack((np.array([baseState[0]]).transpose(), np.array([baseState[1]]).transpose(), np.array([[jointStates[i_joint][0] for i_joint in range(len(jointStates))]]).transpose()))
 	v = np.vstack((np.array([baseVel[0]]).transpose(), np.array([baseVel[1]]).transpose(), np.array([[jointStates[i_joint][1] for i_joint in range(len(jointStates))]]).transpose()))
 	v_dot = (v-v_prev)/dt
 	v_prev = v.copy()
 	
-	####################################################################
 	
-	HQPData = invdyn.computeProblemData(t, q, v)
+	## Conversion (from 8 to 12 DOF) and TSID computation 
+	
+	q12 = np.concatenate((q[:7], np.matrix([0.]), q[7:9], np.matrix([0.]), q[9:11], np.matrix([0.]), q[11:13], np.matrix([0.]), q[13:15]))
+	v12 = np.concatenate((v[:6], np.matrix([0.]), v[6:8], np.matrix([0.]), v[8:10], np.matrix([0.]), v[10:12], np.matrix([0.]), v[12:14]))
+	
+	HQPData = invdyn.computeProblemData(t, q12, v12)
 	
 	sol = solver.solve(HQPData)
 	
 	tau = invdyn.getActuatorForces(sol)
 	dv = invdyn.getAccelerations(sol)
 	
-	vdes += dt*dv
-	qdes = pin.integrate(model, qdes, dt*vdes)
+	
+	## Time incrementation and display update
+	
 	t += dt
 	
-	robot_display.display(q)
+	robot_display.display(q12)
 		
-	####################################################################
+		
+	## Torque controller (conversion from 12 to 8 DOF)
 	
-	# PD Torque controller
-	Kp_PD = 8.0
-	Kd_PD = 0.1
+	torques = np.concatenate((tau[1:3], tau[4:6], tau[7:9], tau[10:12]))
 	
-	torques = tau #+ Kp_PD * (qdes[7:] - q[7:]) + Kd_PD * (vdes[6:] - v[6:])
 	
-	# Saturation to limit the maximal torque
+	## Saturation to limit the maximal torque
+	
 	t_max = 5
-	torques = np.maximum(np.minimum(torques, t_max * np.ones((12,1))), -t_max * np.ones((12,1)))
+	torques = np.maximum(np.minimum(torques, t_max * np.ones((8,1))), -t_max * np.ones((8,1)))
 	
 	return torques, dv[7], dv[10], dv[13], dv[16]
+
+
+########################################################################
+#                             Simulator                                #
+########################################################################
 
 ## Launch the simulation
 
@@ -294,6 +291,9 @@ for i in range (N_SIMULATION):
 		if t_sleep > 0:
 			time.sleep(t_sleep)	
 
+########################################################################
+#                              Results                                 #
+########################################################################
 
 ## Plot the results
 
