6514136be27b932e0f92a4316845df4dea9b70a3
sduminy <sduminy@laas.fr>
2020-01-07 13:35:25 +0100

    Working P_controller for one motor

--- sinController.py
+++ sinController.py
@@ -19,33 +19,53 @@ pin.switchToNumpyMatrix()
 
 
 ########################################################################
-#                          TSID Controller                             #
+#                   Class for a sinus Controller                       #
 ########################################################################
 
 class sinController:
-	def __init__(self, q0, v0, omega, t):
+	
+	def __init__(self, q0, omega):
 		self.omega = omega
-		self.qdes = q0
-		qdes[0] = np.sin(omega*t) + q0[0]
-		self.vdes = v0
-		vdes[0] = omega * np.cos(omega*t)
-		self.ades = v0
-		ades[0] = -omega**2 * np.sin(omega*t)
-	def control(self, qmes, vmes):
+		self.qdes = q0.copy()
+		self.vdes = np.zeros((8,1))
+		self.ades = np.zeros((8,1))
+		
+	####################################################################
+	#                      Torque Control method                       #
+	####################################################################
+	def control(self, qmes, vmes, t):
+		# Definition of qdes, vdes and ades
+		self.qdes[0] = np.sin(self.omega * t)
+		self.vdes[0] = self.omega * np.cos(self.omega * t)
+		self.ades[0] = -self.omega**2 * np.sin(self.omega * t)
+		#print(self.qdes[0], self.vdes[0])
+		
+		# Definition of the solver
 		HQPData = invdyn.computeProblemData(t, self.qdes, self.vdes)
 		self.sol = solver.solve(HQPData)
-		P = 1.0
-		D = 2.0 * np.sqrt(P)
+		
+		# PD Torque controller
+		P = 10.0
+		D = 2.0 * np.sqrt(P)/2
 		tau = P * (self.qdes - qmes) - D * vmes
-		return tau
+		
+		# Saturation to limit the maximal torque
+		t_max = 2.5
+		tau = np.maximum(np.minimum(tau, t_max * np.ones((8,1))), -t_max * np.ones((8,1)))
+	
+		return tau[0]
 
 
-# Parameters of the configuration test
-T = 1e100					# sinus period (s)
+########################################################################
+#                        Parameters definition                         #
+########################################################################
+
+# Parameters of the desired trajectory
+T = 2 * np.pi					# sinus period (s)
 omega = 2 * np.pi / T	# sinus pulsation
  
 # Simulation parameters
-N_SIMULATION = 20000	# number of time steps simulated
+N_SIMULATION = 10000	# number of time steps simulated
 dt = 0.001				# controller time step
 
 t = 0.0  				# time
@@ -53,6 +73,10 @@ t = 0.0  				# time
 # Set the simulation in real time
 realTimeSimulation = True
 
+
+########################################################################
+#             Definition of the Model and TSID problem                 #
+########################################################################
 
 ## Set the paths where the urdf and srdf file of the robot are registered
 
@@ -62,17 +86,14 @@ srdf = modelPath + "/solo_description/srdf/solo.srdf"
 vector = pin.StdVec_StdString()
 vector.extend(item for item in modelPath)
 
-
 ## Create the robot wrapper from the urdf model (without the free flyer)
 
 robot = tsid.RobotWrapper(urdf, vector, False)
-
 
 ## Take the model of the robot and load its reference configuration
 
 model = robot.model()
 pin.loadReferenceConfigurations(model, srdf, False)
-
 
 ## Set the initial configuration vector to the robot configuration straight_standing
 ## And set the initial velocity to zero
@@ -88,7 +109,6 @@ invdyn = tsid.InverseDynamicsFormulationAccForce("tsid", robot, False)
 invdyn.computeProblemData(t, q0, v0)
 # Get the initial data
 data = invdyn.data()
-
 
 ## Initialization of the solver
 
@@ -135,17 +155,24 @@ p.setTimeStep(dt)
 
 
 ########################################################################
-#                      Torque Control function                         #
+#                             Simulator                                #
 ########################################################################
 
-controller = sinController(q0, v0, omega, t)
+myController = sinController(q0, omega)
 
-## Function called from the main loop which computes the inverse dynamic problem and returns the torques
-def callback_torques():
+Qdes = []
+Qmes = []
+Vdes = []
+Vmes = []
+
+for i in range (N_SIMULATION):
 	
-	controller = sinController(q0, v0, omega, t)
-	
-	## Data collection from PyBullet
+	if realTimeSimulation:	
+		time_start = time.time()
+		
+	####################################################################
+	#                 Data collection from PyBullet                    #
+	####################################################################
 	
 	jointStates = p.getJointStates(robotId, revoluteJointIndices) # State of all joints
 	
@@ -153,60 +180,54 @@ def callback_torques():
 	qmes = np.vstack((np.array([[jointStates[i_joint][0] for i_joint in range(len(jointStates))]]).T))
 	vmes = np.vstack((np.array([[jointStates[i_joint][1] for i_joint in range(len(jointStates))]]).T))
 	
-	## Time incrementation
+	####################################################################
+	#         Retrieve the joint torques from the controller           #
+	####################################################################
 	
-	t += dt
-	
-	torques = sinController.control(qmes, vmes)
-	
-		
-	## Saturation to limit the maximal torque
-	
-	t_max = 2.5
-	torques = np.maximum(np.minimum(torques, t_max * np.ones((8,1))), -t_max * np.ones((8,1)))
-	
-	return torques
-
-
-########################################################################
-#                             Simulator                                #
-########################################################################
-
-## Launch the simulation
-
-t_list = []	# list to verify that each iteration of the simulation is less than 1 ms
-
-for i in range (N_SIMULATION):
-	
-	if realTimeSimulation:	
-		time_start = time.time()
-	
-	# Callback Pinocchio to get joint torques
-	jointTorques[0] = callback_torques()[0]
-	
+	jointTorques[0] = myController.control(qmes, vmes, t)
+	print(myController.qdes[0], myController.vdes[0])
+	"""qdes0 = myController.qdes[0]
+	vdes0 = myController.vdes[0]
+	print(qdes0, vdes0)"""
 	# Stop the simulation if the QP problem can't be solved
-	if(controller.sol.status != 0):
-		print ("QP problem could not be solved ! Error code:", sol.status)
+	if(myController.sol.status != 0):
+		print ("QP problem could not be solved ! Error code:", myController.sol.status)
 		break
 	
 	# Set control torque for all joints
 	p.setJointMotorControlArray(robotId, revoluteJointIndices, controlMode=p.TORQUE_CONTROL, forces=jointTorques)
 	
+	# Tracking of the trajectories
+	Qdes.append(myController.qdes[0].copy())
+	Vdes.append(myController.vdes[0].copy())
+	Qmes.append(qmes[0])
+	Vmes.append(vmes[0])
+	
 	# Compute one step of simulation
 	p.stepSimulation()
+	
+	# Time incrementation
+	t += dt
 	
 	if realTimeSimulation:
 		time_spent = time.time() - time_start
 		if time_spent < dt:
 			time.sleep(dt-time_spent)	# ensure the simulation runs in real time
 			
-	t_list.append(time_spent)
 
-## Plot the list of the duration of each iteration
-## It should be less than 0.001 s
+## Plot the tracking of the trajectories
 
 import matplotlib.pylab as plt
-
-plt.plot(t_list, '+k')
+plt.figure()
+plt.subplot(2,1,1)
+plt.plot(Qdes, 'b', label="Desired position")
+plt.plot(Qmes, 'r', label="Measured position")
 plt.grid()
+plt.legend()
+plt.title("Trajectories tracking")
+plt.subplot(2,1,2)
+plt.plot(Vdes, 'b', label="Desired velocity")
+plt.plot(Vmes, 'r', label="Measured velocity")
+plt.grid()
+plt.legend()
 plt.show()
