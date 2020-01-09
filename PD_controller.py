e65c01122897460cc27fda37ecc71ced238d7c44
sduminy <sduminy@laas.fr>
2020-01-09 16:52:43 +0100

    Change the error

--- PD_controller.py
+++ PD_controller.py
@@ -32,21 +32,21 @@ class controller:
 	####################################################################
 	def control(self, qmes, vmes, t):
 		# Definition of qdes, vdes and ades
-		self.qdes = np.sin(self.omega * t) + self.q0
+		self.qdes = 2.0*np.sin(self.omega * t) + self.q0
 		self.vdes = self.omega * np.cos(self.omega * t)
 		self.ades = -self.omega**2 * np.sin(self.omega * t)
 		
 		# PD Torque controller
 		P = np.diag((10.0, 5.0, 10.0, 5.0, 10.0, 5.0, 10.0, 5.0))
-		D = np.diag((3.0, 1.0, 3.0, 1.0, 3.0, 1.0, 3.0, 1.0))
+		D = 0.1*np.diag((3.0, 1.0, 3.0, 1.0, 3.0, 1.0, 3.0, 1.0))
 		tau = np.array(np.matrix(np.diag(P * (self.qdes - qmes) + D * (self.vdes - vmes))).T)
 		
 		# Saturation to limit the maximal torque
 		t_max = 2.5
 		tau = np.maximum(np.minimum(tau, t_max * np.ones((8,1))), -t_max * np.ones((8,1)))
 		
-		self.error = np.linalg.norm(vmes)>1000
-		
+		self.error = self.error or (qmes[0] < -np.pi/2) or (qmes[2] < -np.pi/2) or (qmes[4] < -np.pi/2) or (qmes[6] < -np.pi/2) or (qmes[0] > np.pi/2) or (qmes[2] > np.pi/2) or (qmes[4] > np.pi/2) or (qmes[6] > np.pi/2)
+	
 		return tau
 
 # Parameters of the desired trajectory
@@ -57,4 +57,4 @@ q0 = np.ones((8,1))		# initial configuration
 
 for i in range(8):
 	omega[i] = 1.0
-	q0[i] = 0.2
+	q0[i] = i/20
